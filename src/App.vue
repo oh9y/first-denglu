020672ff5fc5aa06a2dddf5c887c69d829e6dd68
Yanghebin <2305525064@qq.com>
2022-10-11 09:06:49 +0800

    完善登录界面的样式

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,14 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
 import myLogin from "./components/myLogin.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
   <main>
     <myLogin></myLogin>
   </main>
-
-  <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
 .logo {
   display: block;
   margin: 0 auto 2rem;
